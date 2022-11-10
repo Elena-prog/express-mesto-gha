6b5790017be558093d818e0d80fa39c1f1e868f4
Elena Tsivinskaya <elts77@gmail.com>
2022-11-10 23:15:31 +0700

    bug fixing

--- controllers/cards.js
+++ controllers/cards.js
@@ -67,10 +67,14 @@ module.exports.dislikeCard = (req, res) => {
         { $pull: { likes: req.user._id } },
         { new: true }
     )
+    .orFail(new Error('Not Found'))
     .then(card => res.send({data: card}))
     .catch((err) => {
-        if (err.name === 'CastError') {
+        if(err.name === 'Error'){
             res.status(404).send({ message: 'Карточка не найдена'});
+        } else
+        if (err.name === 'CastError') {
+            res.status(400).send({ message: 'Карточка не найдена'});
           } else {
             res.status(500).send({ message: 'Произошла ошибка' });
         }
