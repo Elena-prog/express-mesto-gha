2006e5ea446f4ded62d7d003ac9dbcd389550281
Elena Tsivinskaya <elts77@gmail.com>
2022-11-10 20:04:24 +0700

    done

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const users = require('./routes/users');
+const cards = require('./routes/cards');
+
+const PORT = 3000;
+const app =  express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    req.user = {
+        _id: '636a783c38c75a6b12e004ef'
+    };
+    next();
+})
+app.use('/users', users);
+app.use('/cards', cards);
+
+app.listen(PORT, () => {
+    console.log(`App listened on port ${PORT}`);
+})
+
+
---
+++ controllers/users.js
@@ -0,0 +1,96 @@
+const user = require('../models/user');
+
+module.exports.getUsers = (req, res) => {
+    user.find({})
+    .then(users => res.send({data: users}))
+    .catch((err) => {
+        if (err.name === 'CastError') {
+            res.status(404).send({ message: 'Пользователи не найдены'});
+          } else {
+            res.status(500).send({ message: 'Произошла ошибка' });
+        }
+    })
+}
+
+module.exports.getUser = (req, res) => {
+    user.findById(req.params.userId)
+    .then(user => { 
+        res.send({data: user})
+    })
+    .catch((err) => {
+        if (err.name === 'CastError') {
+            res.status(404).send({ message: 'Пользователь не найден'});
+          } else {
+            res.status(500).send({ message: 'Произошла ошибка'});
+        }
+    })
+}
+
+module.exports.createUser = (req, res) => {
+    const {name, about, avatar} = req.body;
+
+    user.create({name, about, avatar})
+    .then(user => res.send({data: user}))
+    .catch((err) => {
+        if (err.name === 'ValidationError') {
+            res.status(400).send({ message: 'Переданы некорректные данные'});
+          } else {
+            res.status(500).send({ message: 'Произошла ошибка'});
+        }
+    })
+}
+
+module.exports.updateUser = (req, res) => {
+    const {name, about} = req.body;
+
+    user.findByIdAndUpdate(
+        req.user._id,
+        {
+            name,
+            about
+        },
+        {
+            new: true,
+            runValidators: true,
+            upsert: true
+        }
+    )
+    .orFail(()=> {
+        throw new Error('Пользователь не найден');
+    })
+    .then(user => res.send({data: user}))
+    .catch((err) => {
+        if(err.statusCode === 404) {
+            res.status(404).send({ message: 'Пользователь не найден'});
+        }
+        if (err.name === 'ValidationError') {
+            res.status(400).send({ message: 'Переданы некорректные данные'});
+          } else {
+            res.status(500).send({ message: 'Произошла ошибка' });
+        }
+    })
+}
+
+module.exports.updateAvatar = (req, res) => {
+    const {avatar} = req.body;
+
+    user.findByIdAndUpdate(
+        req.user._id,
+        {
+            avatar
+        },
+        {
+            new: true,
+            runValidators: true,
+            upsert: true
+        }
+    )
+    .then(user => res.send({data: user}))
+    .catch((err) => {
+        if (err.name === 'ValidationError') {
+            res.status(400).send({ message: 'Переданы некорректные данные'});
+          } else {
+            res.status(500).send({ message: 'Произошла ошибка' });
+        }
+    })
+}
