07407b9a18f9ae8b57b16de047cbfcd61fdf0f29
Elena Tsivinskaya <elts77@gmail.com>
2022-12-04 12:46:56 +0700

    request.log & error.log

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const handleError = require('./middlewares/handleError');
 const router = require('./routes/index');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,10 +15,12 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   autoIndex: true,
 });
 
+app.use(requestLogger);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', router);
+app.use(errorLogger);
 app.use(errors());
 app.use(handleError);
 
--- errors/bad-request-err.js
+++ errors/bad-request-err.js
@@ -1,5 +1,5 @@
 const { BAD_REQUEST } = require('../constants');
-//console.log('ff');
+
 class BadRequestError extends Error {
   constructor(message) {
     super(message);
